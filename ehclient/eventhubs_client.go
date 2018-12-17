827d22332f1ee83473c4115118b4e1be165d4ec1
Thomas Conte <thomas@conte.com>
2018-12-17 17:26:15 +0100

    Reuse connection to event hub

--- ehclient/eventhubs_client.go
+++ ehclient/eventhubs_client.go
@@ -13,13 +13,25 @@ import (
 // Client posts messages to Event Hubs
 type ehclient struct {
 	connectionString string
+	hub              *eventhub.Hub
 	logger           lager.Logger
 }
 
 // NewEventHubsClient creates a new instance of the Client
 func NewEventHubsClient(connectionString string, logger lager.Logger) common.Client {
+	// Create Event Hubs client
+	hub, err := eventhub.NewHubFromConnectionString(connectionString)
+	if err != nil {
+		logger.Error("ehclient: error creating Event Hubs client", err)
+		return nil
+	}
+
+	//defer hub.Close(context.Background()) //??????
+
+	// Return client
 	return &ehclient{
 		connectionString: connectionString,
+		hub:              hub,
 		logger:           logger,
 	}
 }
@@ -30,16 +42,8 @@ func (c *ehclient) PostData(msg *[]byte, logType string) error {
 	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
 	defer cancel()
 
-	// Create Event Hubs client
-	hub, err := eventhub.NewHubFromConnectionString(c.connectionString)
-	if err != nil {
-		c.logger.Debug("Error creating Event Hub client")
-		return err
-	}
-	defer hub.Close(ctx)
-
 	// Send message
-	err = hub.Send(ctx, eventhub.NewEventFromString(string(*msg)))
+	err := c.hub.Send(ctx, eventhub.NewEventFromString(string(*msg)))
 	if err != nil {
 		c.logger.Debug("Error sending message to Event Hub")
 		return err
@@ -49,19 +53,6 @@ func (c *ehclient) PostData(msg *[]byte, logType string) error {
 }
 
 func (c *ehclient) PostBatchData(batch *[]interface{}, logType string) (int, error) {
-	// Create context
-	ctx := context.Background()
-	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
-	defer cancel()
-
-	// Create Event Hubs client
-	hub, err := eventhub.NewHubFromConnectionString(c.connectionString)
-	if err != nil {
-		c.logger.Debug("Error creating Event Hub client")
-		return 0, err
-	}
-	defer hub.Close(ctx)
-
 	// Assemble batch
 	var evbatch []*eventhub.Event
 	var evbatchSize int
@@ -71,11 +62,15 @@ func (c *ehclient) PostBatchData(batch *[]interface{}, logType string) (int, err
 		evbatch = append(evbatch, eventhub.NewEventFromString(string(j)))
 	}
 
+	// Create context
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
 	// Send batch
 	c.logger.Debug("Sending batch to hub", lager.Data{"size": evbatchSize})
-	err = hub.SendBatch(ctx, &eventhub.EventBatch{Events: evbatch})
+	err := c.hub.SendBatch(ctx, &eventhub.EventBatch{Events: evbatch})
 	if err != nil {
-		c.logger.Debug("Error sending message to Event Hub")
+		c.logger.Error("ehclient: error sending batch to Event Hub", err)
 		return 0, err
 	}
 
